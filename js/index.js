c3bf5ecc095d684e2a3bd1843ab8dba20553c5e3
little_luo <face000623@gmail.com>
2025-05-30 16:37:15 +0800

    貪吃蛇 | 優化程式

--- js/index.js
+++ js/index.js
@@ -1,261 +1,278 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let cellWidth = 10;
 let cellHeight = 10;
-let rows = 30;
-let cols = 30;
-
+const ROWS = 30;
+const COLS = 30;
 
 let p0 = {
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0,
 };
 
-p0.x = canvas.width / 2 - (cols * cellWidth) / 2;
-p0.y = canvas.height / 2 - (rows * cellHeight) / 2;
-
-let snakeBody = [{
-    x: undefined,
-    y: undefined,
-}];
-
-let food = {
-    x:undefined,
-    y:undefined,
-}
-
-let score = 0;
+p0.x = canvas.width / 2 - (COLS * cellWidth) / 2;
+p0.y = canvas.height / 2 - (ROWS * cellHeight) / 2;
 
 const strokeWidth = 1;
-(function init(){
-    // snakeBody[0].x = Math.floor(Math.random() * cols) * cellWidth + p0.x;
-    // snakeBody[0].y = Math.floor(Math.random() * rows) * cellHeight + p0.y;
-    snakeBody[0] = getRandomPosition();
-    food = getRandomPosition();
-    displaySnake(snakeBody);
-    displayGrid();
-    displayFood(food.x,food.y);
-    displayScore();
-})();
-
-let direction = {
+const gameState = {
+  snakeBody: [{}],
+  food: {},
+  score: 0,
+  direction: {
     up: false,
     right: false,
     down: false,
     left: false,
+  },
+  start: false,
+  terminate: false,
+  speed: 100,
+};
+
+const COLOR = {
+  SNAKE: "red",
+  FOOD: "green",
+  SCORE: "black",
+};
+
+(function init() {
+  gameState.snakeBody[0] = getRandomPosition();
+  gameState.food = getRandomPosition();
+  displaySnake(gameState.snakeBody);
+  displayGrid();
+  displayFood(gameState.food.x, gameState.food.y);
+  displayScore();
+})();
+
+window.addEventListener("keydown", function (e) {
+  let keyCode = e.keyCode;
+  if (keyCode >= 37 && keyCode <= 40) {
+    switch (keyCode) {
+      case 37: {
+        updateDirection("left");
+        break;
+      }
+      case 38: {
+        updateDirection("up");
+        break;
+      }
+      case 39: {
+        updateDirection("right");
+        break;
+      }
+      case 40: {
+        updateDirection("down");
+        break;
+      }
+    }
+  }
+});
+
+let speedUp_id = undefined;
+window.addEventListener("keydown", function (e) {
+  let keyCode = e.keyCode;
+  if (keyCode >= 37 && keyCode <= 40) {
+    gameState.start = true;
+    if (gameState.start && speedUp_id === undefined) {
+      speedUp_id = setInterval(speedUp, 5000);
+    }
+  }
+});
+
+function displaySnake(snakeBody) {
+  ctx.fillStyle = COLOR.SNAKE;
+  snakeBody.forEach((body) => {
+    ctx.fillRect(body.x, body.y, cellWidth, cellHeight);
+  });
 }
 
-window.addEventListener('keydown',function(e){
-    let keyCode = e.keyCode;
-    if(keyCode >= 37 && keyCode <= 40){
-        switch(keyCode){
-            case 37: {
-                updateDirection('left');
-                break;
-            }
-            case 38: {
-                updateDirection('up');
-                break;
-            }
-            case 39: {
-                updateDirection('right');
-                break;
-            }
-            case 40: {
-                updateDirection('down');
-                break;
-            }
-        }
-    }
-}) 
+function displayGrid() {
+  ctx.lineWidth = strokeWidth;
+  ctx.beginPath();
+  // 繪製橫線
+  for (let i = 0; i <= ROWS; i++) {
+    ctx.moveTo(p0.x, p0.y + i * cellHeight);
+    ctx.lineTo(p0.x + COLS * cellWidth, p0.y + i * cellWidth);
+    ctx.stroke();
+  }
 
-function displaySnake(snakeBody){
-    ctx.fillStyle = 'red';
-    snakeBody.forEach(body => {
-        ctx.fillRect(body.x,body.y,cellWidth,cellHeight); 
-    });
+  // 繪製直線
+  for (let i = 0; i <= COLS; i++) {
+    ctx.moveTo(p0.x + i * cellWidth, p0.y);
+    ctx.lineTo(p0.x + i * cellWidth, p0.y + ROWS * cellHeight);
+    ctx.stroke();
+  }
+  ctx.closePath();
 }
 
-function displayGrid(){
-    ctx.lineWidth = strokeWidth;
-    ctx.beginPath();
-    // 繪製橫線
-    for(let i = 0; i <= rows; i++){
-        ctx.moveTo(p0.x,p0.y + i * cellHeight);
-        ctx.lineTo(p0.x + cols * cellWidth,p0.y + i * cellWidth);
-        ctx.stroke();
-    }
-
-    // 繪製直線
-    for(let i = 0; i <= cols; i++){
-        ctx.moveTo(p0.x + i * cellWidth, p0.y);
-        ctx.lineTo(p0.x + i * cellWidth, p0.y + rows * cellHeight);
-        ctx.stroke();
-    }
-    ctx.closePath(); 
+function isCollidedX(x) {
+  if (x >= p0.x && x <= p0.x + (COLS - 1) * cellWidth) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function isCollidedY(y) {
+  if (y >= p0.y && y <= p0.y + (ROWS - 1) * cellHeight) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function isCollidedX(x){
-    if(x >= p0.x && x <= p0.x + (cols - 1) * cellWidth){
-        return false;
-    }else{
-        return true;
-    }
-}
-function isCollidedY(y){
-    if(y >= p0.y && y <= p0.y + (rows - 1) * cellHeight){
-        return false;
-    }else{
-        return true;
-    }
-
-}
-
-function updateDirection(str){
-    direction.up = false;
-    direction.right = false;
-    direction.down = false;
-    direction.left = false;
-    if(str !== undefined){
-        direction[str] = true;
-    }
+function updateDirection(str) {
+  gameState.direction.up = false;
+  gameState.direction.right = false;
+  gameState.direction.down = false;
+  gameState.direction.left = false;
+  if (str !== undefined) {
+    gameState.direction[str] = true;
+  }
 }
 
 let terminate = false;
 let newPosition = {
-    x: snakeBody[0].x,
-    y: snakeBody[0].y,
-}
-function updateSnakePosition(){
-    if(direction.left){
-        if(!isCollidedX(snakeBody[0].x)){
-           newPosition.x = snakeBody[0].x - 10;
-        }else{
-            newPosition.x = p0.x;
-            terminate = true;
-        }
-        newPosition.y = snakeBody[0].y;
+  x: gameState.snakeBody[0].x,
+  y: gameState.snakeBody[0].y,
+};
+function updateSnakePosition() {
+  if (gameState.direction.left) {
+    if (!isCollidedX(gameState.snakeBody[0].x)) {
+      newPosition.x = gameState.snakeBody[0].x - 10;
+    } else {
+      newPosition.x = p0.x;
+      terminate = true;
     }
-    if(direction.right){
-        if(!isCollidedX(snakeBody[0].x)){
-             newPosition.x = snakeBody[0].x + 10;
-        }else{
-            newPosition.x = p0.x + (cols - 1) * cellWidth;
-            terminate = true;
-        }
-        newPosition.y = snakeBody[0].y;
+    newPosition.y = gameState.snakeBody[0].y;
+  }
+  if (gameState.direction.right) {
+    if (!isCollidedX(gameState.snakeBody[0].x)) {
+      newPosition.x = gameState.snakeBody[0].x + 10;
+    } else {
+      newPosition.x = p0.x + (COLS - 1) * cellWidth;
+      terminate = true;
     }
-    if(direction.up){
-        if(!isCollidedY(snakeBody[0].y)){
-            newPosition.y = snakeBody[0].y - 10;
-        }else{
-            newPosition.y = p0.y
-            terminate = true;
-        }
-        newPosition.x = snakeBody[0].x;
+    newPosition.y = gameState.snakeBody[0].y;
+  }
+  if (gameState.direction.up) {
+    if (!isCollidedY(gameState.snakeBody[0].y)) {
+      newPosition.y = gameState.snakeBody[0].y - 10;
+    } else {
+      newPosition.y = p0.y;
+      terminate = true;
     }
-    if(direction.down){
-        if(!isCollidedY(snakeBody[0].y)){
-            newPosition.y = snakeBody[0].y + 10;
-        }else{
-            newPosition.y = p0.y + (rows - 1) * cellHeight;
-            terminate = true;
-        }
-        newPosition.x = snakeBody[0].x;
+    newPosition.x = gameState.snakeBody[0].x;
+  }
+  if (gameState.direction.down) {
+    if (!isCollidedY(gameState.snakeBody[0].y)) {
+      newPosition.y = gameState.snakeBody[0].y + 10;
+    } else {
+      newPosition.y = p0.y + (ROWS - 1) * cellHeight;
+      terminate = true;
     }
-    // 這裡根據是否吃到食物決定是否 pop
-    snakeBody.unshift({...newPosition});
-    if (!collidedWithFood()) {
-        snakeBody.pop();
-    }else{
-        changeScore();
-    }
-    
-    displaySnake(snakeBody); 
+    newPosition.x = gameState.snakeBody[0].x;
+  }
+  // 這裡根據是否吃到食物決定是否 pop
+  gameState.snakeBody.unshift({ ...newPosition });
+  if (!collidedWithFood()) {
+    gameState.snakeBody.pop();
+  } else {
+    changeScore();
+  }
+
+  displaySnake(gameState.snakeBody);
 }
 
-function stopGame(){
-    if(terminate){
-        alert('結束遊戲');
-        clearInterval(id);
-        // 刷新頁面
-        window.location.reload();
+function stopGame() {
+  if (terminate) {
+    alert("結束遊戲");
+    clearInterval(id);
+    clearInterval(speedUp_id);
+    // 刷新頁面
+    window.location.reload();
+  }
+}
+
+function displayFood(x, y) {
+  ctx.fillStyle = COLOR.FOOD;
+  ctx.fillRect(x, y, cellWidth, cellHeight);
+}
+function getRandomPosition() {
+  let position = {
+    x: Math.floor(Math.random() * COLS) * cellWidth + p0.x,
+    y: Math.floor(Math.random() * ROWS) * cellHeight + p0.y,
+  };
+  return position;
+}
+
+function collidedWithFood() {
+  if (
+    gameState.snakeBody[0].x === gameState.food.x &&
+    gameState.snakeBody[0].y === gameState.food.y
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function collideWithSelf() {
+  for (let i = 1; i < gameState.snakeBody.length; i++) {
+    if (
+      gameState.snakeBody[0].x === gameState.snakeBody[i].x &&
+      gameState.snakeBody[0].y === gameState.snakeBody[i].y
+    ) {
+      gameState.snakeBody = gameState.snakeBody.slice(0, i);
+      return true;
     }
+  }
+  return false;
 }
 
-function displayFood(x,y){
-    ctx.fillStyle = 'green';
-    ctx.fillRect(x,y,cellWidth,cellHeight);
-}
-function getRandomPosition(){
-    let position = {
-        x: Math.floor(Math.random() * cols) * cellWidth + p0.x,
-        y: Math.floor(Math.random() * rows) * cellHeight + p0.y,
-    }
-    return position;
-}
-
-function collidedWithFood(){
-    if(snakeBody[0].x === food.x && snakeBody[0].y === food.y){
-        return true;
-    }
-    return false;
+function displayScore() {
+  ctx.fillStyle = COLOR.SCORE;
+  ctx.font = "20px serif";
+  ctx.fillText(
+    "".concat("目前分數 : ", gameState.score),
+    p0.x + COLS * cellWidth,
+    p0.y - 1 * cellHeight
+  );
+  ctx.textAlign = "right";
 }
 
-function collideWithSelf(){
-    for(let i = 1; i < snakeBody.length; i++){
-        if(snakeBody[0].x === snakeBody[i].x && snakeBody[0].y === snakeBody[i].y){
-            snakeBody = snakeBody.slice(0, i);
-            return true;
-        }
-    }
-    return false;
+function changeScore() {
+  gameState.score += 10;
 }
 
-function displayScore(){
-    ctx.fillStyle = 'black';
-    ctx.font = '20px serif';
-    ctx.fillText(score,p0.x + cols * cellWidth,p0.y - 1 * cellHeight);
-    ctx.textAlign = 'right';
+function speedUp() {
+  // console.log(gameState.speed);
+  if (gameState.speed > 40) {
+    gameState.speed -= 5;
+    clearInterval(id);
+    id = setInterval(move, gameState.speed);
+  }
 }
 
-function changeScore(){
-    score += 10;
+function move() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  updateSnakePosition();
+
+  stopGame();
+
+  if (collidedWithFood()) {
+    gameState.food = getRandomPosition();
+    displayFood(gameState.food.x, gameState.food.y);
+  }
+  displayFood(gameState.food.x, gameState.food.y);
+
+  displayGrid();
+
+  collideWithSelf();
+
+  displayScore();
 }
-
-function speedUp(){
-    console.log(speed);
-    if(speed > 40){
-        speed -= 5;
-        clearInterval(id);
-        id = setInterval(move,speed);
-    }
-}
-
-function move(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    
-    updateSnakePosition();    
-    
-    stopGame();
-    
-    if(collidedWithFood()){
-        food = getRandomPosition();
-        displayFood(food.x,food.y);
-    }
-    displayFood(food.x,food.y);
-    
-    displayGrid();
-
-    collideWithSelf();
-    
-    displayScore();
-
-}
-let speed = 100;
-let id = setInterval(move,speed);
-const speedUpId = setInterval(speedUp,5000);
+let id = setInterval(move, gameState.speed);
